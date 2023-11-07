2742278bbd3b90f19979f88a835b68bbd6c4e8ea
GildasTessier <140600513+GildasTessier@users.noreply.github.com>
2023-11-07 16:23:39 +0100

    create end of style

--- task.php
+++ task.php
@@ -1,11 +1,22 @@
 <li class="task">
-    <button>⬆
-    </button>
+    <div class="first-line-task">
+    
+    <button class="btn-priority" id="btn-priority">UP⇧</button>
     <h3 class="title-task"> Name Task</h3>
+    
+    <form>
+        <input class="checkbox-task" type="checkbox" name="checkbox" id="checkbox-task">
+    </form>
+    </div>
+    <div class="back-line"
     <p class="date-task"> 22-10-2023</p>
     <p class="text-task">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit, distinctio cumque. 
         Similique velit animi quidem illo ab non dolorem culpa incidunt saepe fugit aperiam placeat, 
         esse natus rerum porro doloribus! </p>
-    <button class="btn-mod-task">Modifier</button>
-    <button class="btn-del-task">Supprimer</button>
+    <div class="btns-task">
+        <button class="btn-mod-task">Modifier</button>
+        <button class="btn-del-task">Supprimer</button>
+    </div>
+    </div>
 </li>
+
