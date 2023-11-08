29348d23a36784d713c33d0032d428901530d3c0
Aurélien Guelle <aurelien.guelle@gmail.com>
2023-11-08 16:01:18 +0100

    delete file php unused

--- create_task.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo '<li>
-    <form action="" method="post" class="create-task">
-        <input class="title-create-task" type="text" name="new_task" id="task-text" >
-        <input class="btn-add-task" name="btn-task" type="submit" value="Add new task">
-        <input type="hidden" name="token" value="'.$_SESSION['token'].'">
-        </form>
-        </li>';
-        ?>
