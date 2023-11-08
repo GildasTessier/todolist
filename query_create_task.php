15e340bad11eca94d97b9565d7f8613c3c9efa02
GildasTessier <140600513+GildasTessier@users.noreply.github.com>
2023-11-08 13:29:28 +0100

    add nb priority on crate task

--- query_create_task.php
+++ query_create_task.php
@@ -7,12 +7,17 @@ if(isset($_POST['new_task'])) {
 
         if(strlen($_POST['new_task']) > 0) {                        
 $query = $dbCo->prepare(" INSERT INTO task (name_task, date_create, state_task)
-                            VALUES (:new_task, :day_now, 0);");
+                            VALUES (:new_task, :day_now, 0);
+                            UPDATE task SET priority_task = id_task   WHERE name_task = :new_task AND date_create = :day_now; 
+                            ");
+
             $isQueryOk = $query->execute([
             'new_task' => strip_tags($_POST['new_task']), 
             'day_now' => date('Y-m-d h:i:s')]
-
+            // $query = $dbCo->prepare(" UPDATE task SET priority_task = id_task   WHERE name_task = :new_task AND date_create = :day_now ");
+            // $isQueryOk = $query->execute();
         );
+
             if($isQueryOk && $query->rowCount()=== 1) {
                 $msg = 'La tache à été ajouté à la liste';
             };
